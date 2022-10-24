d5ad2441e66905402ded93c61f00af8b1d9c430d
Nihat Halici <nihat.halici@gmail.com>
2022-10-24 10:23:49 +0200

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,22 @@
+class Settings:
+	"""A class to store all settings for Alien Invasion."""
+
+	def __init__(self):
+		"""Initialize the game's static settings."""
+		pass
+
+	def initialize_easy_dynamic_settings(self):
+		"""Initialize settings for easy modus that change throughout the game."""
+		pass
+
+	def initialize_medium_dynamic_settings(self):
+		"""Initialize settings for medium modus that change throughout the game."""
+		pass
+
+	def initialize_hard_dynamic_settings(self):
+		"""Initialize settings for hard modus that change throughout the game."""
+		pass
+
+	def increase_speed(self):
+		"""Increase speed settings and alien point values."""
+		pass
