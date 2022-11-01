d2c6848a9f5ca45fabf638c940e41d0834dd8e3f
Nihat Halici <nihat.halici@gmail.com>
2022-11-01 11:42:32 +0100

    Add files via upload

---
+++ alien_invasion.py
@@ -0,0 +1,74 @@
+import sys
+
+class AlienInvasion:
+
+	def __init__(self):
+		pass
+
+	def run_game(self):
+		pass
+
+	def _check_events(self):
+		pass
+
+	def _check_easy_play_button(self, mouse_pos):
+		pass
+
+	def _check_medium_play_button(self, mouse_pos):
+		pass
+
+	def _check_hard_play_button(self, mouse_pos):
+		pass
+
+	def _check_keydown_events(self, event):
+		pass
+
+	def _start_game(self):
+		pass
+
+	def _check_keyup_events(self, event):
+		pass
+
+	def _fire_bullet(self):
+		pass
+
+	def _update_bullets(self):
+		pass
+
+	def _check_bullet_alien_collisions(self):
+		pass
+
+	def _start_new_level(self):
+		pass
+
+	def _update_aliens(self):
+		pass
+
+	def _check_aliens_bottom(self):
+		pass
+
+	def _ship_hit(self):
+		pass
+
+	def _create_fleet(self):
+		pass
+
+	def _create_alien(self):
+		pass
+
+	def _check_fleet_edges(self):
+		pass
+
+	def _change_fleet_direction(self):
+		pass
+
+	def _update_screen(self):
+		pass
+
+
+
+
+
+if __name__ == '__main__':
+	ai = AlienInvasion()
+	ai.run_game()
