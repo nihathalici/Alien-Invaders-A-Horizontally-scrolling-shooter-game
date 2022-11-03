981fcfb31654570cf64faebebead33975b4535db
Nihat Halici <nihat.halici@gmail.com>
2022-11-03 05:59:44 +0100

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,9 +15,33 @@ from alien import Alien
 import sounds
 
 class AlienInvasion:
+	"""Overall class to manage game assets and behavior."""
 
 	def __init__(self):
-		pass
+		"""Initialize the game, and create game resources."""
+		pygame.init()
+		self.settings = Settings()
+
+		self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+		self.settings.screen_width = self.screen.get_rect().width()
+		self.settings.screen_height = self.screen.get_rect().height
+		pygame.display.set_caption("Alien Invasion")
+
+		# Create an instance to store game statistics.
+		self.stats = GameStats(self)
+		self.sb = Scoreboard(self)
+		self.ship = Ship(self)
+		self.achterground = Background(self)
+		self.bullets = pygame.sprite.Group()
+		self.aliens = pygame.sprite.Group()
+
+		self._create_fleet()
+
+		# Make the Play buttons.
+		self.play_easy_button = EasyButton(self, "Easy mode")
+		self.play_medium_button = MediumButton(self, "Medium mode")
+		self.play_hard_button = HardButton(self, "Hard mode")
+
 
 	def run_game(self):
 		pass
